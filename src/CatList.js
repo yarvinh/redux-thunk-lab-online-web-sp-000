71e7a7157854bc72cca51f456ea3e7bde997e551
yarvinh <yarvinhhernandez@gmail.com>
2021-06-29 20:02:09 -0400

    commit tree 62919e1a48a7274e80f746e3667c47029a5e7a0a

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,26 @@
 // write your CatList component here
+import React, { Component } from 'react';
+
+
+class CatList extends Component {
+renderCatList = ()=>{
+ return this.props.catPics.map(cat =>{
+     return (
+        
+           <img key={cat.id} src={cat.url} alt="Girl in a jacket" width="500" height="600"/>
+       
+        )
+ })
+
+}
+
+  render() {
+    return (
+      <div>
+            {this.renderCatList()}
+      </div>
+    );
+  }
+};
+
+export default CatList 
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,19 @@
 // write and export your action creator function here
+
+// fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+//   return response.json()
+// }).then(responseJSON => {
+//   // instead of logging here, call dispatch and send the cat JSON data to your store
+//   console.log(responseJSON.images)
+// })
+
+export const fetchCats = () => {
+    return (dispatch) => {
+      dispatch({ type: 'LOADING_CATS'})
+      fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+        return response.json()
+      }).then(responseJSON => {
+        dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
+      })
+    }
+  }
